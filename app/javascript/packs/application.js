570ea34f36eea100a949e69da04336f56f39cc2b
rikako <jc180215b@gmail.com>
2022-06-07 23:15:23 +0900

    modify:import_tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../css/tailwindcss.css";
+import "stylesheets/application"
 
 Rails.start()
 ActiveStorage.start()
@@ -15,13 +17,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "../css/tailwindcss.css";
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import "stylesheets/application"
