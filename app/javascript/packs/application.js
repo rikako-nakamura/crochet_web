032189383fcebcc53d6f66ddcf04bccede47718a
rikako <jc180215b@gmail.com>
2022-06-08 23:08:44 +0900

    fix:tailwind_設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,16 +6,12 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "../css/tailwindcss.css";
-import "stylesheets/application"
+import "stylesheets/application.css"
+import "../css/tailwindcss.css"
+
 
 Rails.start()
 ActiveStorage.start()
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
