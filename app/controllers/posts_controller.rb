3d48d83fcff0ba8cffd886391b2fb3159fecca4d
rikako <jc180215b@gmail.com>
2022-06-12 12:46:05 +0900

    modify:new,create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,17 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    post = Post.new(post_params)
+    redirect_to posts_url, notice: "投稿しました！"
+  end
+
   def edit
+  end
+
+  private
+
+  def post_params
+    params.permit(:body)
   end
 end
