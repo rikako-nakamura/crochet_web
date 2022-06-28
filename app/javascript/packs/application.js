b9c4f90e422dadddd326659996d74e5f3a24e4d4
rikako <jc180215b@gmail.com>
2022-06-28 20:25:01 +0900

    modify:swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,5 @@ ActiveStorage.start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require swiper/swiper-bundle.js
+//= require swiper.js
